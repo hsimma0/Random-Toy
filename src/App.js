1a766511334dd4b0d2e9f2a84e4677744e224f10
Herivin Simmasalam <hsimma0@gmail.com>
2022-06-09 18:26:43 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const API_URL = 'https://dog.ceo/api/breeds/image/random'
+
+//THIS is useState, you pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.
+  const [dog, setDog] =useState(null)
+
+const getDog = async () => {
+  const response = await fetch(API_URL);
+}
+
+//THIS IS THE HTML RETURN
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Random Dog Photo Generator</h1>
+      <input type="button" value="Get Random Dog" onClick={getDog} />
+      <img src={dog} alt="random dog" />
     </div>
+    
   );
 }
 
