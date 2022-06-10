bb0b1f950cd261887e79c5c118488240e21a88c3
Herivin Simmasalam <hsimma0@gmail.com>
2022-06-10 09:45:39 -0400

    Final CSS Edits

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import {useState} from 'react';
-import DogDisplay from './components/DogDisplay';
-import './App.css';
+import { useState } from "react";
+import DogDisplay from "./components/DogDisplay";
+import "./App.css";
 
 function App() {
-  const API_URL = 'https://dog.ceo/api/breeds/image/random'
+  const API_URL = "https://dog.ceo/api/breeds/image/random";
 
-//THIS is useState, you pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.
-  const [dog, setDog] =useState(null)
+  //THIS is useState, you pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.
+  const [dog, setDog] = useState(null);
 
-const getDog = async () => {
-  const response = await fetch(API_URL);
-  
-  const data = await response.json();
-    setDog(data.message)
-}
+  const getDog = async () => {
+    const response = await fetch(API_URL);
 
-//THIS IS THE HTML RETURN
+    const data = await response.json();
+    setDog(data.message);
+  };
+
+  //THIS IS THE HTML RETURN
   return (
-    <div className='app'>
-      <h1>Random Dog Photo Generator</h1>
-      <input type="button" value="Get Random Dog" onClick={getDog} />
-      <DogDisplay dog= {dog} />
+    <div className="app">
+      <div className="tag">Random Dog Photo Generator</div>
+      <div className="submit">
+        <input type="button" value="Get Random Dog" onClick={getDog} />
+      </div>
+      <div className="dogImg">
+        <DogDisplay dog={dog} />
+      </div>
     </div>
-    
   );
 }
 
